005bb48257c6625f6d1cd04dc0f68240f7c54179
Ritesh Birthal <riteshbirthal@gmail.com>
2025-04-15 00:32:45 +0530

    Solution 14-Apr-2025

---
+++ LeetCode Solutions/Year-2025/April/14-April/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int n = arr.size(), res = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                for(int k = j+1; k < n; k++){
+                    if(abs(arr[j]-arr[i])<=a && abs(arr[k]-arr[j])<=b && abs(arr[k]-arr[i])<=c)
+                        res++;
+                }
+            }
+        }
+        return res;
+    }
+};
