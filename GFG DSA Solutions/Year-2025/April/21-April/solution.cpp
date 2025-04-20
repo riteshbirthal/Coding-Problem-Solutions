6aaab08c3d2ac983f461190ec4601a9122e18d72
Ritesh Birthal <riteshbirthal@gmail.com>
2025-04-21 00:21:04 +0530

    Solution 21-Apr-2025

---
+++ GFG DSA Solutions/Year-2025/April/21-April/solution.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+        // code here
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        for(int i = 0; i < n; i++)
+            if(arr[i]!=i+1)
+                return i+1;
+        return n+1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.missingNum(a) << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
