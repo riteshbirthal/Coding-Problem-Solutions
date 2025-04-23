0c216759ad46b007be5fe1288c6de29cc8d6bc19
Ritesh Birthal <riteshbirthal@gmail.com>
2025-04-24 00:07:40 +0530

    Solution 24-Apr-2025

---
+++ GFG DSA Solutions/Year-2025/April/24-April/solution.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    int getSingle(vector<int> &arr) {
+        // code here
+        unordered_map<int, int> u;
+        for(int x : arr){
+            u.find(x)==u.end() ? u[x] = 1 : u[x] += 1;
+            if(u[x]==3)
+                u.erase(x);
+        }
+        return u.begin()->first;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.getSingle(arr);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
