f4d424719d2b5a3d826864347b35a35851a6c3f4
riteshbirthal <riteshbirthal@gmail.com>
2026-01-12 22:31:59 +0530

    Implement minTimeToVisitAllPoints function

---
+++ LeetCode Solutions/Year-2026/January/12-January/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int res = 0, n = points.size();
+        for(int i = 1; i < n; i++)
+            res += max(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]));
+        return res;
+    }
+};
