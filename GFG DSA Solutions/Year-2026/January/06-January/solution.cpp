ba20167224f30b6611343406a21f93e89d8b3292
riteshbirthal <riteshbirthal@gmail.com>
2026-01-19 23:38:43 +0530

    Add maxSubarrayXOR function to Solution class
    Implement maxSubarrayXOR function to find maximum XOR of subarrays of size k.

---
+++ GFG DSA Solutions/Year-2026/January/06-January/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int maxSubarrayXOR(vector<int>& arr, int k) {
+        // code here
+        int res = 0, n = arr.size(), curr = 0;
+        for(int i = 0; i < n; i++){
+            curr ^= arr[i];
+            if(i<k-1)
+                continue;
+            res = max(res, curr);
+            curr ^= arr[i-k+1];
+        }
+        return res;
+    }
+};
