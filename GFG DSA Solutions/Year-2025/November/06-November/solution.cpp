75d881cf340966cfb4f5142c02179213dfc95d85
Ritesh Birthal <riteshbirthal@gmail.com>
2026-01-27 23:39:23 +0530

    GFG Problem: Ways To Tile A Floor
    GFG Daily Problem: 06 November 2025

---
+++ GFG DSA Solutions/Year-2025/November/06-November/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int numberOfWays(int n) {
+        // code here
+        vector<int> dp(n+1);
+        dp[0] = 1, dp[1] = 1;
+        for(int i = 2; i < n+1; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
+    }
+};
