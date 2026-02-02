7228749a6262b54ee7f6b4de036a9c1190f9c371
Ritesh Birthal <riteshbirthal@gmail.com>
2026-02-02 22:44:24 +0530

    GFG Problem: K Sized Subarray Maximum
    GFG Daily Problem: 01 February 2026

---
+++ GFG DSA Solutions/Year-2026/February/01-February/soution.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+  public:
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        // code here
+        map<int, int> mp;
+        vector<int> res;
+        int n = arr.size();
+        for(int i = 0; i < n; i++){
+            if(i<k-1){
+                mp[arr[i]]++;
+                continue;
+            }
+            mp[arr[i]]++;
+            res.push_back(mp.rbegin()->first);
+            mp[arr[i-k+1]]==1 ? mp.erase(arr[i-k+1]) : mp[arr[i-k+1]]--;
+        }
+        return res;
+    }
+};
