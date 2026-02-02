9c3ec793acf0c6b9a39526aa1ab6f7d0f3ffeb73
Ritesh Birthal <riteshbirthal@gmail.com>
2026-02-02 22:42:50 +0530

    GFG Problem: Max Circular Subarray Sum
    GFG Daily Problem: 02 February 2026

---
+++ GFG DSA Solutions/Year-2026/February/02-February/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int maxCircularSum(vector<int> &arr) {
+        // code here
+        int ans = INT_MIN, n = arr.size(), sum = 0, mx = INT_MIN;
+        for(auto x : arr){
+            sum = sum<=0 ? x : sum + x;
+            ans = max(ans, sum);
+        }
+        sum = 0;
+        vector<int> vec = {0};
+        for(auto x : arr){
+            sum += x;
+            mx = max(mx, sum);
+            vec.push_back(mx);
+        }
+        sum = 0;
+        for(int i = n-1; i > 0; i--){
+            sum += arr[i];
+            ans = max(ans, sum + vec[i]);
+        }
+        return ans;
+    }
+};
