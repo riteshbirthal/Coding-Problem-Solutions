82c39c7ae5cffa11bc8052bf4bb52874ad3afc89
Ritesh Birthal <riteshbirthal@gmail.com>
2026-02-04 00:23:06 +0530

    GFG Problem: Last Moment Before All Ants Fall Out
    GFG Daily Problem: 04 February 2026

---
+++ GFG DSA Solutions/Year-2026/February/04-February/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        // code here
+        int res = 0;
+        for(int x : left)
+            res = max(res, x);
+        for(int x : right)
+            res = max(res, n-x);
+        return res;
+    }
+};
