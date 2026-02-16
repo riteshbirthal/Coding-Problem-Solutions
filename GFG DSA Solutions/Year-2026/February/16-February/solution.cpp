972a1152db9a0720fedbb7c3fa31b9191d213564
Ritesh Birthal <riteshbirthal@gmail.com>
2026-02-16 20:14:56 +0530

    Added solution for GFG Problem: Meeting Rooms
    GFG Daily Problem: 16 February 2026

---
+++ GFG DSA Solutions/Year-2026/February/16-February/solution.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        // Your Code Here
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        for(int i = 1; i < n; i++){
+            if(arr[i][0]<arr[i-1][1])
+                return false;
+        }
+        return true;
+    }
+};
