53d9a35634223dfd66af9a2ed7e8a7d7d04d71a9
Ritesh Birthal <riteshbirthal@gmail.com>
2026-02-24 21:38:06 +0530

    Added solution for GFG Problem: Count Subarrays with given XOR
    GFG Daily Problem: 22 February 2026

---
+++ GFG DSA Solutions/Year-2026/February/22-February/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) {
+        // code here
+        unordered_map<int, int> u;
+        u[0] = 1;
+        int XOR = 0, res = 0;
+        for(int x : arr){
+            XOR ^= x;
+            if(u.find(XOR^k)!=u.end()) res += u[XOR^k];
+            u.find(XOR)==u.end() ? u[XOR] = 1 : u[XOR] += 1;
+        }
+        return res;
+    }
+};
